a8b10337b04362490e902a17d58a9e32c9b93ab5
3fs03 <eraygaming.tr26@gmail.com>
2022-06-04 08:28:55 +0300

    Update Program.cs

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -9,6 +9,21 @@ namespace Game
             Console.WriteLine("Hello World!");
             Console.WriteLine("Didaka log");
             Console.WriteLine("3rayski was here");
+            Console.Title = "ASCII Art";
+            string title = @"
+ ____                                     ______    __    ___             
+/\  _`\                                  /\__  _\__/\ \__/\_ \            
+\ \ \L\_\ __      ___     ___   __  __   \/_/\ \/\_\ \ ,_\//\ \      __   
+ \ \  _\/'__`\  /' _ `\  /'___\/\ \/\ \     \ \ \/\ \ \ \/ \ \ \   /'__`\ 
+  \ \ \/\ \L\.\_/\ \/\ \/\ \__/\ \ \_\ \     \ \ \ \ \ \ \_ \_\ \_/\  __/ 
+   \ \_\ \__/.\_\ \_\ \_\ \____\\/`____ \     \ \_\ \_\ \__\/\____\ \____\
+    \/_/\/__/\/_/\/_/\/_/\/____/ `/___/> \     \/_/\/_/\/__/\/____/\/____/
+                                    /\___/                                
+                                    \/__/                                 
+                                                                 ";
+
+            Console.WriteLine(title);
+            Console.Read();
         }
     }
 }
