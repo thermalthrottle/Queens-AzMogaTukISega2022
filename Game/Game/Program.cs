a0a5dea8663ad10a94b84578e71f4ea1c8972202
Dimitar Todorov <36155679+thermalthrottle@users.noreply.github.com>
2022-06-04 09:09:39 +0300

    Base file structure

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -6,9 +6,6 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Didaka log");
-            Console.WriteLine("3rayski was here");
             Console.Title = "Queens";
             string title = @"
               @
