675951cb3a6d2ba0f8e128b226413cba18e32e12
Dimitar Todorov <36155679+thermalthrottle@users.noreply.github.com>
2022-06-04 09:21:32 +0300

    Board class base properties and methods

--- Game/Game/GameLogic/Board.cs
+++ Game/Game/GameLogic/Board.cs
@@ -8,5 +8,28 @@ namespace Game.GameLogic
 {
     internal class Board
     {
+        private char[,] board;
+
+        public char[,] GameBoard { get => board; }
+
+        public Board(int width, int height)
+        {
+            board = new char[width, height];
+        }
+
+        public void Place(int x, int y)
+        {
+
+        }
+
+        private bool ValidateIfOccupied()
+        {
+            return false;
+        }
+
+        private bool CheckIFGameFinished()
+        {
+            return false;
+        }
     }
 }
